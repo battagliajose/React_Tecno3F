20a519b68d2a45b0e9f268cc1795aa3c549dc076
José Battaglia <battagliajose@gmail.com>
2026-04-14 07:32:28 -0300

    Agregadas funcionalidad de Favoritos y Busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-import "./App.css";
+import "./styles/reset.css";
+import "./styles/normalize.css";
+import "./styles/main.css";
+
+import { useState } from "react";
 import CardGrid from "./components/CardGrid.jsx";
 import CustomHeader from "./components/CustomHeader.jsx";
+import Search from "./components/Search.jsx";
 import { catalogo } from "./data/data.js";
 
 function App() {
+  const [catalogoConFavoritos, setCatalogoConFavoritos] = useState(
+    catalogo.map((item) => ({
+      ...item,
+      favorito: false,
+    })),
+  );
+
+  const [textoBusqueda, setTextoBusqueda] = useState("");
+
+  const handleSetFavorito = (id) => {
+    setCatalogoConFavoritos((prevCatalogo) =>
+      prevCatalogo.map((item) =>
+        item.id === id ? { ...item, favorito: !item.favorito } : item,
+      ),
+    );
+  };
+
+  const handlersOnSearch = (event) => {
+    setTextoBusqueda(event.target.value.toLowerCase());
+  };
+
+  const catalogoFiltrado = catalogoConFavoritos.filter((item) =>
+    item.titulo.toLowerCase().includes(textoBusqueda),
+  );
+
   return (
-    <>
+    <main>
       <CustomHeader />
-      <CardGrid />
-    </>
+      <Search onChange={handlersOnSearch} />
+      <CardGrid catalogo={catalogoFiltrado} onSetFavorito={handleSetFavorito} />
+    </main>
   );
 }
 
