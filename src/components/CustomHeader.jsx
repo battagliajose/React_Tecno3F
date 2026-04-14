f8c5f5f4df742ca70bdc400454aff63d354518c9
José Battaglia <battagliajose@gmail.com>
2026-04-14 07:23:15 -0300

    Agregado componente CustomHeader

--- src/components/CustomHeader.jsx
+++ src/components/CustomHeader.jsx
@@ -1,7 +1,12 @@
+import gamerlogo from "../assets/gamerlogo.png";
+import "../styles/custom-header.css";
+
 export default function CustomHeader() {
   return (
     <header>
-      <h1>Catálogo de Juegos</h1>
+      <img src={gamerlogo} alt="Logo Gamer" />
+      <h1>Tecno.3F - Catálogo</h1>
+      <img src={gamerlogo} alt="Logo Gamer" />
     </header>
   );
 }
